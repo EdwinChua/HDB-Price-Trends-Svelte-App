5d2c7f6de2a2ad77f7bf17339c280a0fd804359c
Edwin Chua <3dw1nchu4@gmail.com>
2023-06-25 02:05:01 +0800

    first rudimentary implementation complete

--- src/components/flatDataComponent.svelte
+++ src/components/flatDataComponent.svelte
@@ -1,88 +1,188 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-    import { HDB_Resale_Flat_Record } from '../models/record';
-	export let flatType:string;
+	import { afterUpdate, onMount } from 'svelte';
+	import { HDB_Resale_Flat_Record } from '../models/record';
+	export let flatType: string;
 	export let data: HDB_Resale_Flat_Record[];
 
-    function formatDate(monthYearString:string) {
-        const [year, month] = monthYearString.split('-');
-        // change 01 to Jan, 02 to Feb, etc.
-        const monthString = new Date(`${year}-${month}-01`).toLocaleString('en-us', { month: 'short', year: 'numeric' });
-        return monthString;
-    }
+	let myChart;
+	let tableData = [];
 
-    function drawChart(){
-        const ctx = document.getElementById('myChart');
-        const myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: data.filter((item) => item.flat_type === flatType).map((item) => formatDate(item.month)),
-                datasets: [{
-                    label: 'Resale Price',
-                    data: data.filter((item) => item.flat_type === flatType).map((item) => item.resale_price),
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    }
+    // console.log = function() {}
 
-    onMount(drawChart)
+	function refreshComponentData() {
+		tableData = loadData();
+		drawChart();
+	}
+
+	function formatDate(monthYearString: string) {
+		const [year, month] = monthYearString.split('-');
+		// change 01 to Jan, 02 to Feb, etc.
+		const monthString = new Date(`${year}-${month}-01`).toLocaleString('en-us', {
+			month: 'short',
+			year: 'numeric'
+		});
+		return monthString;
+	}
+
+	function getLabels() {
+		return [...new Set(tableData.map((item) => item.month))];
+	}
+
+	function generateDatasetFilteredByFlatType() {
+		let chartLabels = getLabels();
+		// get distinct flat types
+		let flatTypes = [...new Set(data.map((item) => item.flat_type))];
+		if (flatType !== 'All') flatTypes = [flatType];
+		// filter data by each flat type
+		const ds = flatTypes.map((flatType) => {
+			const filteredData = tableData
+				.filter((item) => item.flat_type === flatType)
+				.sort(sortDataByMonth);
+			return {
+				flatType: flatType,
+				data: filteredData.map((item) => item.resale_price),
+				temp: filteredData.map((item) => item.month)
+			};
+		});
+		// console.log(ds);
+
+		let dses = {};
+		for (let ds_ of ds) {
+			// console.log(ds_);
+			// combine data and temp into a map
+			let map = new Map();
+			for (let i = 0; i < ds_.data.length; i++) {
+				if (map.has(ds_.temp[i])) {
+					let val = map.get(ds_.temp[i]).val;
+					let count = map.get(ds_.temp[i]).count;
+					map.set(ds_.temp[i], {
+						count: count + 1,
+						val: parseFloat(val) + parseFloat(ds_.data[i])
+					});
+				} else map.set(ds_.temp[i], { count: 1, val: parseFloat(ds_.data[i]) });
+			}
+			dses[ds_.flatType] = { map: map };
+		}
+		// console.log(dses);
+		for (let flatType of flatTypes) {
+			let arr = [];
+			for (let label of chartLabels) {
+				let temp_val = dses[flatType].map.get(label) ?? null;
+				if (temp_val != null) {
+					arr.push(temp_val.val / temp_val.count);
+				} else {
+					arr.push(null);
+				}
+				// arr.push(dses[flatType].map.get(label) ?? null);
+			}
+			// console.log(arr);
+			dses[flatType].data = arr;
+		}
+		// console.log(dses);
+
+		const datasets = flatTypes.map((flatType) => {
+			// console.log(dses[flatType].data);
+			const filteredData = tableData
+				.filter((item) => item.flat_type === flatType)
+				.sort(sortDataByMonth);
+			return {
+				label: flatType,
+				data: dses[flatType].data,
+				spanGaps: true,
+				// temp: filteredData.map((item) => item.month),
+
+				// backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+				// borderColor: ['rgba(255, 99, 132, 1)'],
+				borderWidth: 1
+			};
+		});
+		// console.log(datasets);
+		return datasets;
+	}
+
+	function filterData(flatType, filteredData) {
+		return filteredData.map((item) => {
+			// console.log(flatType, item);
+			return item.resale_price;
+		});
+	}
+
+	function drawChart() {
+		if (myChart) myChart.destroy();
+		const ctx = document.getElementById('myChart');
+		let chartObj = {
+			type: 'line',
+			data: {
+				labels: getLabels(),
+				datasets: generateDatasetFilteredByFlatType()
+			},
+			options: {
+				responsive: true,
+				maintainAspectRatio: false,
+				scales: {
+					y: {
+						beginAtZero: false
+					}
+				}
+			}
+		};
+		myChart = new Chart(ctx, chartObj);
+	}
+
+	function loadData() {
+		if (flatType !== 'All')
+			return data.filter((item) => item.flat_type === flatType).sort(sortDataByMonth);
+		else return data.sort(sortDataByMonth);
+	}
+
+	function sortDataByMonth(a, b) {
+		return getDateObj(a.month).getTime() - getDateObj(b.month).getTime();
+		function getDateObj(monthYearString: string) {
+			const [year, month] = monthYearString.split('-');
+			return new Date(`${year}-${month}-01`);
+		}
+	}
+	onMount(drawChart);
+	afterUpdate(refreshComponentData);
 </script>
 
-    <div style="max-height:200px">
-        <canvas id="myChart"></canvas>
-    </div>
-        <h2>{flatType} Table</h2>
-        <div class="table-responsive small">
-            <table class="table table-striped table-sm">
-            <thead>
-                <tr>
-                    <th scope="col">Block</th>
-                    <th scope="col">Flat Model</th>
-                    <th scope="col">Flat Type</th>
-                    <th scope="col">Floor Area (sqm)</th>
-                    <th scope="col">Lease Commence Date</th>
-                    <th scope="col">Month</th>
-                    <th scope="col">Remaining Lease</th>
-                    <th scope="col">Resale Price</th>
-                    <th scope="col">Storey Range</th>
-                    <th scope="col">Street Name</th>
-                    <th scope="col">Town</th>
-                </tr>
-            </thead>
-            <tbody>
-                {#each data.filter((item) => item.flat_type === flatType) as item}
-                    <tr>
-                        <td>{item.block}</td>
-                        <td>{item.flat_model}</td>
-                        <td>{item.flat_type}</td>
-                        <td>{item.floor_area_sqm}</td>
-                        <td>{item.lease_commence_date}</td>
-                        <td>{formatDate(item.month)}</td>
-                        <td>{item.remaining_lease}</td>
-                        <td>{item.resale_price}</td>
-                        <td>{item.storey_range}</td>
-                        <td>{item.street_name}</td>
-                        <td>{item.town}</td>
-                    </tr>
-                {/each}
-            </tbody>
-        </table>
-        </div>
-
-
+<div style="height:300px">
+	<canvas id="myChart" />
+</div>
+<h2>{flatType} Table</h2>
+<div class="table-responsive small">
+	<table class="table table-striped table-sm">
+		<thead>
+			<tr>
+				<th scope="col">Block</th>
+				<th scope="col">Flat Model</th>
+				<th scope="col">Flat Type</th>
+				<th scope="col">Floor Area (sqm)</th>
+				<th scope="col">Lease Commence Date</th>
+				<th scope="col">Month</th>
+				<th scope="col">Remaining Lease</th>
+				<th scope="col">Resale Price</th>
+				<th scope="col">Storey Range</th>
+				<th scope="col">Street Name</th>
+				<th scope="col">Town</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each tableData as item}
+				<tr>
+					<td>{item.block}</td>
+					<td>{item.flat_model}</td>
+					<td>{item.flat_type}</td>
+					<td>{item.floor_area_sqm}</td>
+					<td>{item.lease_commence_date}</td>
+					<td>{formatDate(item.month)}</td>
+					<td>{item.remaining_lease}</td>
+					<td>{item.resale_price}</td>
+					<td>{item.storey_range}</td>
+					<td>{item.street_name}</td>
+					<td>{item.town}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,44 @@
 	import { HDB_Resale_Flat_Record } from '../models/record';
 	import Table from '../components/flatDataComponent.svelte';
 
+  const maxApiCalls = 10;
 	let data: HDB_Resale_Flat_Record[] = []; // Variable to store the retrieved data
-	let activeTab = ''; // Variable to store the active tab
-
+	let activeTab = 'All'; // Variable to store the active tab
+  let baseApi = `https://data.gov.sg`;
   let query:string = 'FARRER PK RD';
+  let distinctFlatTypes = [];
+
+   async function refreshComponent(){
+    await fetchData();
+    await setActiveTab('All');
+    distinctFlatTypes = getDistinctFlatTypes();
+  }
+
+  function submitForm(){
+    refreshComponent();
+  }
+
 	async function fetchData() {
+    console.log(query);
 		try {
 			const response = await fetch(
-				`https://data.gov.sg/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&q=${query}`
+				`${baseApi}/api/action/datastore_search?resource_id=f1765b54-a209-4718-8d38-a39237f502b3&q=${query}`
 			);
 			const result = await response.json();
-			console.log(result.result.records);
+      let apiCallCount = 1;
 			data = result.result.records as HDB_Resale_Flat_Record[];
+      console.log(result.result.total)
+      if (result.result._links.next && data.length < result.result.total) {
+        let result2 = result;
+        while(result2.result._links.next && data.length < result2.result.total && apiCallCount < maxApiCalls){
+          const response2 = await fetch(`${baseApi}/${result2.result._links.next}`);
+          result2 = await response2.json();
+          apiCallCount++;
+          data = data.concat(result2.result.records as HDB_Resale_Flat_Record[]);
+        }
+        // window.alert(data.length)
+        console.warn(data)
+      }
 		} catch (error) {
 			console.error('Error fetching data:', error);
 		}
@@ -27,114 +53,28 @@
 		activeTab = tab;
 	}
 	// Call the fetchData function when the component is mounted
-	onMount(fetchData);
+	onMount(refreshComponent);
 </script>
-
+<form on:submit|preventDefault={submitForm}>
+<input type="text" bind:value={query}/><button type="submit">Search</button>
+</form>
 <h1>Results for: {query}</h1>
 <ul class="nav">
 	{#if data.length > 0}
-		{#each getDistinctFlatTypes() as flatType}
+    <li class="nav-item">
+      <a class="nav-link {'All' === activeTab ? 'active' : ''}" aria-current="page" href="#"	on:click={() => setActiveTab('All')}>All</a>
+    </li>
+		{#each distinctFlatTypes as flatType}
 			<li class="nav-item">
-				<a
-					class="nav-link {flatType === activeTab ? 'active' : ''}"
-					aria-current="page"
-					href="#"
-					on:click={() => setActiveTab(flatType)}>{flatType}</a
-				>
+				<a class="nav-link {flatType === activeTab ? 'active' : ''}" aria-current="page" href="#"	on:click={() => setActiveTab(flatType)}>{flatType}</a>
 			</li>
 		{/each}
 	{/if}
 </ul>
 
-<div class="container-fluid">
+<div class="container">
   <div class="row">
-    <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
-      <div class="offcanvas-lg offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
-          <button type="button" class="btn btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
-                Dashboard
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                Orders
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                Products
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                Customers
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                Reports
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                Integrations
-              </a>
-            </li>
-          </ul>
-
-          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
-            <span>Saved reports</span>
-            <a class="link-secondary" href="#" aria-label="Add a new report">
-            </a>
-          </h6>
-          <ul class="nav flex-column mb-auto">
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                Current month
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                Last quarter
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                Social engagement
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                Year-end sale
-              </a>
-            </li>
-          </ul>
-
-          <hr class="my-3">
-
-          <ul class="nav flex-column mb-auto">
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                Settings
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2" href="#">
-                Sign out
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
-    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <main class="col-md-9  col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
@@ -149,11 +89,8 @@
         </div>
       </div>
 {#if data.length > 0}
-	{#each getDistinctFlatTypes() as flatType}
-		{#if flatType === activeTab}
-			<Table {flatType} {data} />
-		{/if}
-	{/each}
+  <Table flatType={activeTab} {data} />
+
 {:else}
 	<p>Loading data...</p>
 {/if}
